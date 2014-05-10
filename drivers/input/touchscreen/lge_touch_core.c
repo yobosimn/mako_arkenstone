837585fe25744eb673032455de69d4d9ac8ff14f
barome <romain.semanaz@gmail.com>
2014-05-10 10:38:41 +0200

    Revert "drivers: input: 2w: add options for enable/disable 2w with power button"

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -44,9 +44,6 @@
 #include <linux/input/doubletap2wake.h>
 #endif
 #endif
-#ifdef CONFIG_PWRKEY_SUSPEND
-#include <linux/input/pmic8xxx-pwrkey.h>
-#endif
 
 struct touch_device_driver*     touch_device_func;
 struct workqueue_struct*        touch_wq;
@@ -2083,10 +2080,6 @@ static void touch_early_suspend(struct early_suspend *h)
         prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
-#ifdef CONFIG_PWRKEY_SUSPEND
-	if (pwrkey_pressed)
-		prevent_sleep = false;
-#endif
 	if (unlikely(touch_debug_mask & DEBUG_TRACE))
 		TOUCH_DEBUG_MSG("\n");
 
@@ -2136,10 +2129,6 @@ static void touch_late_resume(struct early_suspend *h)
         prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
-#ifdef CONFIG_PWRKEY_SUSPEND
-	if (pwrkey_pressed)
-		prevent_sleep = false;
-#endif
 	if (unlikely(touch_debug_mask & DEBUG_TRACE))
 		TOUCH_DEBUG_MSG("\n");
 
@@ -2152,7 +2141,6 @@ static void touch_late_resume(struct early_suspend *h)
 
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
         if (!prevent_sleep) {
-		pwrkey_pressed = false;
 #endif
 	        touch_power_cntl(ts, ts->pdata->role->resume_pwr);
 
