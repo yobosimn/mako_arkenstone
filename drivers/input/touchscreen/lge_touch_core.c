fb0107197a206769b2824e65687288ac81f01952
barome <romain.semanaz@gmail.com>
2014-05-10 10:26:04 +0200

    Revert: drivers: input: 2w: fix late resume problems

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -2201,8 +2201,6 @@ static void touch_late_resume(struct early_suspend *h)
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
         bool prevent_sleep = false;
-	int int_pin = 0;
-	int next_work = 0;
 #endif
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
         prevent_sleep = (s2w_switch == 1);
@@ -2247,24 +2245,6 @@ static void touch_late_resume(struct early_suspend *h)
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 	} else {
 		prevent_sleep_disable_irq_wake(ts->client->irq);
-		/* Interrupt pin check after IC init - avoid Touch lockup */
-		if (ts->pdata->role->operation_mode == INTERRUPT_MODE) {
-			int_pin = gpio_get_value(ts->pdata->int_pin);
-			next_work = atomic_read(&ts->next_work);
-
-			if (unlikely(int_pin != 1 && next_work <= 0)) {
-				TOUCH_INFO_MSG("WARN: [2w]Interrupt pin is low (Lockup detected) - next_work: %d, try_count: %d]\n",
-						next_work, ts->ic_init_err_cnt);
-				pr_warn("touch core: [2w]disable irqs!\n");
-				disable_irq(ts->client->irq);
-				pr_warn("touch core: [2w]release all Touch events!\n");
-				release_all_ts_event(ts);
-				pr_warn("touch core: [s2w]enable irqs!\n");
-				enable_irq(ts->client->irq);
-				pr_warn("touch core: [s2w]force IC init!\n");
-				touch_ic_init(ts);
-				}
-			}
 		if (s2w_error) {
 			s2w_error = false;
 			TOUCH_ERR_MSG("soft resetting device\n");
